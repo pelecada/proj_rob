86639d7217db9f5ba0c1b1a75fce3e45e1ad15fb
pelecada <pelecada@fel.cvut.cz>
2023-11-09 14:18:22 +0100

    aa

--- start.py
+++ start.py
@@ -6,14 +6,38 @@ sys.path.append(sys.path[0].replace("proj","robtoolbox"))
 import numpy as np
 from robotBosch import robotBosch
 from CRS_commander import Commander
-from robotics_toolbox.core import SE2
+#from robotics_toolbox.core import SE2
 
-a = SE2()
+#a = SE2()
 rob = robotBosch()
 c = Commander(rob) 
-c.open_comm("/dev/ttyUSB0")
+c.open_comm("/dev/ttyUSB0",speed = 19200)
+#c.release()
+#c.reset_motors()
 c.init()
+"""
+print("home")
+c.wait_ready()
 
+resp = c.query('COORDAP')
+resp = np.fromiter(map(int, resp.split(',')), int)
+print(resp)
+
+tgt = np.array([10,10,-10,10])
+deg = np.array([0,0,0,0]) #deg, deg, mm, deg
+for i in range(4):
+    #c.wait_ready()
+    deg[i] = tgt[i]
+    irc = c.anglestoirc(deg)
+    c.coordmv(irc)
+    print("sent", i)
+
+resp = c.query('COORDAP')
+resp = np.fromiter(map(int, resp.split(',')), int)
+print(resp)
+#print(c.irctoangles(resp[]))
 #do stuff
-
+"""
+c.release()
+c.rcon.close()
 print("OK")
