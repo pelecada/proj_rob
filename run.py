432fab65d0dade3f60e717111fd4427191628c29
pelecada <pelecada@fel.cvut.cz>
2023-11-28 15:20:21 +0100

    rename + quickstart

---
+++ run.py
@@ -0,0 +1,20 @@
+from ctu_bosch_sr450 import RobotBosch
+import numpy as np
+
+from line import Line
+from plan import Plan
+
+robot = RobotBosch()  # initialize object without connection to the robot
+robot.initialize(home=False)
+robot.soft_home()
+
+p = Line([[0.45,0],[0.2,0],[0.35,0.1]]) #y = 0 : x <0.25,0,45>, h <0.17,0.5>
+
+configs = Plan(p,0.01)
+
+for q in configs:
+    #robot.wait_for_motion_stop()
+    robot.move_to_q(q)
+
+robot.soft_home()
+robot.close()
--- start.py
+++ start.py
@@ -1,19 +1,5 @@
 from ctu_bosch_sr450 import RobotBosch
-import numpy as np
-
-from line import Line
-from plan import Plan
 
 robot = RobotBosch()  # initialize object without connection to the robot
-robot.initialize(home=False)
-robot.soft_home()
-
-p = Line([[0.45,0],[0.2,0],[0.35,0.1]]) #y = 0 : x <0.25,0,45>, h <0.17,0.5>
-
-configs = Plan(p,0.01)
-
-for q in configs:
-    robot.move_to_q(q)
-
-robot.soft_home()
+robot.initialize()
 robot.close()
